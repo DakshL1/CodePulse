e687d0abdcdcca4a2f315608e2e90c9b8db2f8fd
Daksh <lohiadaksh2613@gmail.com>
2025-05-01 01:18:56 +0530

    hh

--- client/src/api/judgeZeroApi.js
+++ client/src/api/judgeZeroApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// import KEY from './judgeZeroKey';
-const KEY = 'df275444c3mshe175bad7459e6f1p16a68ejsn8ae31e5fe911';
+import KEY from './judgeZeroKey';
 
 const createBulkSubmission = async (id, code, testCases) => {
   console.log("Creating bulk submission:", id, code, testCases);
